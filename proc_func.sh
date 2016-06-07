f95bea3ddc56e33fe8352d73735d4f1596c4bd3e
Kai Hwang <kai.hwang@gmail.com>
2016-06-07 13:00:10 -0700

    forgot to bandpass.

--- proc_func.sh
+++ proc_func.sh
@@ -2,33 +2,35 @@
 
 WD='/home/despoB/kaihwang/Rest/TMS_Thalamus/'
 
-for s in 608; do
+for s in 609; do
 
-	for site in Pre; do
+	for site in Pre S1 IPL; do
 
-		for run in 1 2; do
+		for run in 1 2 3; do
 
 			if [ -d ${WD}/${s}/${site}/Run${run} ]; then
 
 				cd ${WD}/${s}/${site}/Run${run}
 
-				if [ ! -e ${WD}/${s}/${site}/Run${run}/nswktm_functional_4.nii.gz ]; then
+				if [ ! -e ${WD}/${s}/${site}/Run${run}/brnswktm_functional_4.nii.gz ]; then
 					
 					preprocessFunctional -dicom "IM*" \
 					-mprage_bet ${WD}/${s}/Pre/MPRAGE/mprage_bet.nii.gz \
 					-warpcoef ${WD}/${s}/Pre/MPRAGE/mprage_warpcoef.nii.gz \
 					-func_refimg ${WD}/${s}/${site}/SBRef/*.dcm \
 					-tr 1.0 \
+					-bandpass_filter 0.009 0.08 \
 					-rescaling_method 100_voxelmean \
 					-template_brain MNI_2mm \
 					-func_struc_dof bbr \
 					-warp_interpolation spline \
 					-constrain_to_template y \
 					-4d_slice_motion \
-					-no_hp \
-					-cleanup \
 					-custom_slice_times detect \
 					-delete_dicom archive \
+					-motion_censor fd=0.3,dvars=20 \
+					-nuisance_file nuisance_regressors.txt \
+					-nuisance_regression 6motion,d6motion,csf,dcsf,wm,dwm \
 					-smoothing_kernel 4
 					#preprocessFunctional -resume
 
